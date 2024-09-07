77af8424480e9f396f3418832b49016ce36f54d4
Maikel Monet <monet1530@gmail.com>
2024-09-07 14:56:58 +0100

    Moved all conversion methods to a static class

---
+++ ConverterLibrary/ConversionMethods.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterLibrary
+{
+    public static class ConversionMethods
+    {
+        // TODO - Change all returning calculations to two decimal places.
+
+        public static string MilesToKilometers(double units)
+        {
+            string kilometers;
+
+            return kilometers = (units * 1.609).ToString();
+        }
+
+        public static string KilometersToMiles(double units)
+        {
+            string miles;
+                
+            return miles = (units / 1.609).ToString();
+        }
+
+        public static string PoundsToKilograms(double units)
+        {
+            string kilograms;
+
+            return kilograms = (units / 2.205).ToString();
+        }
+
+        public static string KilogramsToPounds(double units)
+        {
+            string pounds;
+
+            return pounds = (units * 2.205).ToString();
+        }
+
+        public static string CelsiusToFahrenheit(double units)
+        {
+            string fahrenheit;
+
+            return fahrenheit = ((units * 1.8) + 32).ToString();
+        }
+
+        public static string FahrenheitToCelsius(double units)
+        {
+            string celsius;
+
+            return celsius = ((units - 32) / 1.8).ToString();
+        }
+    }
+}
--- UnitConverter/UnitConverterDashboard.cs
+++ UnitConverter/UnitConverterDashboard.cs
@@ -1,8 +1,10 @@
+using ConverterLibrary;
+
 namespace UnitConverter
 {
     public partial class UnitConverterDashboard : Form
     {
-        private List<string> methods = new List<string>();
+        private readonly List<string> methods = new List<string>();
 
         public UnitConverterDashboard()
         {
@@ -33,7 +35,8 @@ namespace UnitConverter
                 MessageBox.Show("Please enter a valid amount");
 
             }
-                      return canConvert;
+           
+            return canConvert;
         }
 
         private void WireUpDropDownList()
@@ -54,62 +57,24 @@ namespace UnitConverter
             switch (conversionMethodDropDownBox.SelectedItem)
             {
                 case "Miles to Kilometers":
-                    MilesToKilometers(double.Parse(unitInput.Text));
+                    unitOutput.Text = ConversionMethods.MilesToKilometers(double.Parse(unitInput.Text));
                     break;
                 case "Kilometers to Miles":
-                    KilometersToMiles(double.Parse(unitInput.Text));
+                    unitOutput.Text = ConversionMethods.KilometersToMiles(double.Parse(unitInput.Text));
                     break;
                 case "Pounds to Kilograms":
-                    PoundsToKilograms(double.Parse(unitInput.Text));
+                    unitOutput.Text = ConversionMethods.PoundsToKilograms(double.Parse(unitInput.Text));
                     break;
                 case "Kilograms to Pounds":
-                    KilogramsToPounds(double.Parse(unitInput.Text));
+                    unitOutput.Text = ConversionMethods.KilogramsToPounds(double.Parse(unitInput.Text));
                     break;
                 case "Celcius to Farenheit":
-                    CelsiusToFahrenheit(double.Parse(unitInput.Text));
+                    unitOutput.Text = ConversionMethods.CelsiusToFahrenheit(double.Parse(unitInput.Text));
                     break;
                 case "Fahrenheit to Celsius":
-                    FahrenheitToCelsius(double.Parse(unitInput.Text));
+                    unitOutput.Text = ConversionMethods.FahrenheitToCelsius(double.Parse(unitInput.Text));
                     break;
             }
         }
-
-        // TODO - Change all returning calculations to two decimal places.
-        private void MilesToKilometers(double units)
-        {
-            double kilometers = units * 1.609;
-            unitOutput.Text = kilometers.ToString();
-        }
-
-        private void KilometersToMiles(double units)
-        {
-            double miles = units / 1.609;
-            unitOutput.Text = miles.ToString();
-        }
-
-        private void PoundsToKilograms(double units)
-        {
-            double kilograms = units / 2.205;
-            unitOutput.Text = kilograms.ToString();
-        }
-
-        private void KilogramsToPounds(double units)
-        {
-            double pounds = units * 2.205;
-            unitOutput.Text = pounds.ToString();
-        }
-
-        private void CelsiusToFahrenheit(double units)
-        {
-            double fahrenheit = units * 1.8 + 32;
-            unitOutput.Text = fahrenheit.ToString();
-        }
-
-        private void FahrenheitToCelsius(double units)
-        {
-            double celsius = (units - 32) / 1.8;
-            unitOutput.Text = celsius.ToString();
-        }
-
     }
 }
